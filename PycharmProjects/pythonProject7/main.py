d01fc478285dd5868fefdd8c2b6bb8820568aaa6
KyleJCode <147545166+KyleJCode@users.noreply.github.com>
2023-11-09 12:33:38 -0500

    Delete PycharmProjects/pythonProject7 directory

--- PycharmProjects/pythonProject7/main.py
+++
@@ -1,6 +0,0 @@
-condition = True
-while condition == True:
-    name = input("What is your username?")
-    password = input("What is your password?")
-    if name == "azim" and password == "gators123":
-        break
